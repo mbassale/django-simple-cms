643038b85aa83ad5230698e95edc1543bd3f942d
Marco Bassaletti <bassaletti@gmail.com>
2019-02-12 01:46:32 -0300

    Show posts from database.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Marco Bassaletti',
-        'title': 'Blog Post 1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
-                   'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
-                   'nisi ut aliquip ex ea commodo consequat.',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
-                   'labore et dolore magna aliqua.',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
