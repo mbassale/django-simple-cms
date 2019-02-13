63d322d82d3606b90a5a076b64e312912dd406d6
Marco Bassaletti <bassaletti@gmail.com>
2019-02-13 00:33:03 -0300

    Login and Logout views with routes.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- users/views.py
+++ users/views.py
@@ -8,9 +8,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for %s' % (username,))
-            return redirect('blog-home')
+            messages.success(request, 'Your account has been created! You are now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
