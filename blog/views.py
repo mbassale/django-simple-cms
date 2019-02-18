ffdf17c1570ddeb7ec5dd2d24257095160aaa070
Marco Bassaletti <bassaletti@gmail.com>
2019-02-18 17:27:15 -0300

    User Posts: display profile full name.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,7 @@ class UserPostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
         return context
 
 
